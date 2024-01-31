cd7249e90443c45660d8154754c806c4752ca730
tejasveedighe <tejasveedighe@gmail.com>
2024-01-31 13:44:30 +0530

    Person class with Overloading and static variable

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.*;
+class Person {
+    String name;
+    int id = 0;
+    /*
+    Static variable will not have its memory reallocated when new object is created
+    of Person class, also it will retain its value throughout the entire program
+    * */
+    static int peopleCount = 0;
+    public Person(String name) {
+        this.name = name;
+        this.id++;
+        peopleCount++;
+    }
+
+    // Overloading the constructor of Person class
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+        peopleCount++;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         // write the code here
-        System.out.println("Hello world");
     }
 }
